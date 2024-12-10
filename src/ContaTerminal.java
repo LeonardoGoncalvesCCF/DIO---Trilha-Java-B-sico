540cc60c1e82ca213b622117788ccdd8c6f35deb
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2024-12-10 18:28:30 -0300

    Codigo refatorado metodo cadastrar

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -6,62 +6,91 @@ public class ContaTerminal {
 	public static void main(String[] args) {
 
 		// Variaveis
-		String agencia;
+		/*String agencia;
 		int numeroConta;
 		String nomeCliente;
 		BigDecimal saldo;
+		*/
+		Scanner entrada = new Scanner(System.in); // instanciando 
 
-		Scanner entrada = new Scanner(System.in);
-
-		System.out.println("----------------------------");
+		System.out.println("|--------------------------|");
 		System.out.println("| Bem vindo ao Banco       |");
-		System.out.println("| Escolha a opção desejada | ");
+		System.out.println("| Escolha a opção desejada |");
 		System.out.println("| 1. Abrir uma conta       |");
 		System.out.println("| 2. Para sair do sistema  |");
-		System.out.println("----------------------------");
+		System.out.println("|--------------------------|");
 
 		// iniciando opçao de Menu e pegando informação do menu
 		int opcao = entrada.nextInt();
 
 		switch (opcao) {
 		case 1:
-			System.out.println("----------------------------------------");
-			System.out.println("| Por favor digite o numero da agencia |");
-			System.out.println("----------------------------------------");
-
-			agencia = entrada.next();
-
-			System.out.println("| Digete agora o numero da conta |");
-
-			numeroConta = entrada.nextInt();
-
-			System.out.println("| Digite o Nome do Cliente | ");
-
-			nomeCliente = entrada.next().toUpperCase(); //Pegando nome cliente e tratando para caixa alta
-
-			System.out.println("| Insira o saldo do Cliente |");
-
-			saldo = entrada.nextBigDecimal();
-
-			System.out.println("Olá " + nomeCliente
-					+ ", obrigado por criar uma conta em nosso banco, sua agência é "
-					+ agencia + ", conta " + numeroConta + " e seu saldo "
-					+ saldo + " já está disponível para saque");
-
-			break;
+			
+			registrarCliente();
+		break;
 
 		case 2:
-			System.out.println("------------------");
-			System.out.println("Saindo do programa");
-			System.out.println("------------------");
+			System.out.println("|--------------------|");
+			System.out.println("| Saindo do programa |");
+			System.out.println("|--------------------|");
+			
+		
 			break;
 
 		default:
-			System.out.println(" Opção Invalida ");
+			System.out.println("|----------------|");
+			System.out.println("| Opção Invalida |");
+			System.out.println("|----------------|");
 			break;
-
+ 
 		}
-
+		
+		entrada.close(); //fechando scanner
+		
+		
 	}
+	
+	//Metodo para Registrar Cliente
+	public static void registrarCliente() {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("|--------------------------------------|");
+		System.out.println("| Por favor digite o numero da agencia |");
+		System.out.println("|--------------------------------------|");
+
+		String agencia = entrada.next(); // pegando a agencia
+		
+		System.out.println("|--------------------------------|");
+		System.out.println("| Digete agora o numero da conta |");
+		System.out.println("|--------------------------------|");
+		
+		int numeroConta = entrada.nextInt(); // pegando Numero da Conta
+		
+		System.out.println("|--------------------------|");
+		System.out.println("| Digite o Nome do Cliente |");
+		System.out.println("|--------------------------|");
+
+		String nomeCliente = entrada.next().toUpperCase(); //Pegando nome cliente e tratando para caixa alta
+		
+		System.out.println("|---------------------------|");
+		System.out.println("| Insira o saldo do Cliente |");
+		System.out.println("|---------------------------|");
+
+
+		BigDecimal saldo = entrada.nextBigDecimal(); // pegando saldo da conta
+		
+		System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
+
+		System.out.println("Olá " + nomeCliente
+				+ ", obrigado por criar uma conta em nosso banco, sua agência é "
+				+ agencia + ", conta " + numeroConta + " e seu saldo "
+				+ saldo + " já está disponível para saque");
+		System.out.println("-----------------------------------------------------------------------------------------------------------------------------");	
+		
+		
+	}
+	
+	
 
 }
